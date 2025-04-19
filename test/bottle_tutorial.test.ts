b1c1343aad50eacae00aeee5b8617af9a0da77e3
Mike Card <mpcard64@gmail.com>
2025-04-19 17:23:13 -0500

    Initial commit

---
+++ test/bottle_tutorial.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BottleTutorial from '../lib/bottle_tutorial-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/bottle_tutorial-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BottleTutorial.BottleTutorialStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
